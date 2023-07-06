14e4b99bb522ce72ea52b7b017822794ce9d3e80
Iñaki Armendariz <iniakitoo@gmail.com>
2023-07-06 19:28:49 -0300

    first commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+function Card({ name, status, species, gender, origin, image }) {
+  return (
+    <div>
+      <button onClick={() => console.log("Botón X clicado")}>X</button>
+      <h2>{name}</h2>
+      <h2>{status}</h2>
+      <h2>{species}</h2>
+      <h2>{gender}</h2>
+      <h2>{origin}</h2>
+      <img src={image} alt="" />
+    </div>
+  );
+}
+
+export default Card;
