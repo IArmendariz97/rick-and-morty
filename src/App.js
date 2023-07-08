32d1512e7b38a800b8f62a5fe350b73d0834f660
Iñaki Armendariz <iniakitoo@gmail.com>
2023-07-07 23:08:43 -0300

    en progreso

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import './App.css';
-import Card from './components/Card.jsx';
-import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
-import characters, { Rick } from './data.js';
+import React, { useState } from "react";
+import style from "./App.module.css";
+import Cards from "./components/Cards/Cards.jsx";
+import Nav from "./components/Navbar/Nav";
+import axios from "axios";
+import Detail from "./components/Detail/Detail";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-   return (
-      <div className='App'>
-         <SearchBar onSearch={(characterID) => window.alert(characterID)} />
-         <Cards characters={characters} />
-         <Card
-            id={Rick.id}
-            name={Rick.name}
-            status={Rick.status}
-            species={Rick.species}
-            gender={Rick.gender}
-            origin={Rick.origin.name}
-            image={Rick.image}
-            onClose={() => window.alert('Emulamos que se cierra la card')}
-         />
-      </div>
-   );
+  function onSearch(dato) {
+    // agrega personajes a characters
+    axios(`https://rickandmortyapi.com/api/character/${dato}`)
+      .then((respuesta) => {
+        if (respuesta.data.name) {
+          // antes de agregar busco si "ya existe". Como lo harias?
+          // tu codigo aquí:
+          // if("yaExiste") return
+          setCharacters((oldChars) => [...oldChars, respuesta.data]);
+        } else {
+        }
+      })
+      .catch((err) => window.alert("¡No hay personajes con este ID!"));
+  }
+
+  function onClose(id) {
+    setCharacters(
+      characters.filter((pj) => {
+        return pj.id !== Number(id);
+      })
+    );
+  }
+
+  const [characters, setCharacters] = useState([]); // [{}]
+
+  // const location = useLocation();
+  // console.log("location", location);
+
+  // let navigate = useNavigate()
+  // function handle(){
+  //   navigate("/algo")
+  // }
+
+  return (
+    <div className={style.App}>
+      <Nav onSearch={onSearch} />
+      <Routes>
+        {/* <Route path="*" element={<Nav onSearch={onSearch} />} /> */}
+        <Route path="/" element={<h1>Welcome</h1>} />
+        <Route
+          path="/home"
+          element={<Cards characters={characters} onClose={onClose} />}
+        />
+        <Route path="/about" element={<h1>Soy el about</h1>} />
+        <Route path="/character/:id" element={<Detail />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
